d37c51766781fddf6deefe1cc6c19272d3abb05f
Lesley <lesleylai@lesleylai.info>
2020-07-25 20:43:23 -0600

    Fix build error on windows

--- include/beyond/utils/force_inline.hpp
+++ include/beyond/utils/force_inline.hpp
@@ -1,7 +1,7 @@
 #ifndef BEYOND_CORE_UTILS_FORCE_INLINE_HPP
 #define BEYOND_CORE_UTILS_FORCE_INLINE_HPP
 
-#ifdef __MSVC__
+#if _MSC_VER
 #define BEYOND_FORCE_INLINE __forceinline
 #else
 #define BEYOND_FORCE_INLINE inline __attribute__((always_inline))
