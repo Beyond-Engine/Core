0b2c621a45fd9d632c005e5d251a0c7fb880e21d
Lesley Lai <lesley@lesleylai.info>
2020-11-17 08:43:20 -0700

    Clean up the code a bit

--- include/beyond/utils/conversion.hpp
+++ include/beyond/utils/conversion.hpp
@@ -1,4 +1,22 @@
-#ifndef RASTERIZER_CONVERSION_HPP
-#define RASTERIZER_CONVERSION_HPP
+#ifndef BEYOND_CORE_UTILS_CONVERSION_HPP
+#define BEYOND_CORE_UTILS_CONVERSION_HPP
 
-#endif // RASTERIZER_CONVERSION_HPP
+#include <cstdint>
+
+#include "force_inline.hpp"
+
+namespace beyond {
+
+constexpr BEYOND_FORCE_INLINE auto to_i32(float value) noexcept
+{
+  return static_cast<int>(value);
+}
+
+constexpr BEYOND_FORCE_INLINE auto to_f32(int value) noexcept
+{
+  return static_cast<float>(value);
+}
+
+} // namespace beyond
+
+#endif // BEYOND_CORE_UTILS_CONVERSION_HPP
